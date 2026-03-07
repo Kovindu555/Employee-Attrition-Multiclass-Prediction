afc98a7a14fceb0ad0e12b7843e8bc5ae362b0fb
Kovindu555 <kovichansi@gmail.com>
2026-03-07 16:07:41 +0530

    code cleanse

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, Response
 import pickle
 # ------------------------------ Library Corner -----------------------------
 
-# ---------------------------------- Tools ----------------------------------
+# ------------------------------ Secondary Tools ----------------------------
 def risk_pred(p):
     if p < 0.25:
         return 'Low Risk'
@@ -34,11 +34,11 @@ def clean_dataset(employee_dataset):
     dataset_directory = 'test_csv/' + dataset_name
     df = pd.read_csv(dataset_directory)
 
-    # ------------- DroppingUnwantedColumns -------------
+    # -- DroppingUnwantedColumns --
     dropping_columns = ['Over18', 'EmployeeCount', 'StandardHours']
     df.drop(dropping_columns, axis=1, inplace=True)
 
-    # ------------- Encoding Department and Gender -------------
+    # -- Encoding Department and Gender --
     df['Department_Cardiology'] = (df['Department'] == 'Cardiology') * 1
     df['Department_Maternity']  = (df['Department'] == 'Maternity') * 1
     df['Department_Neurology'] = (df['Department'] == 'Neurology') * 1
@@ -48,12 +48,12 @@ def clean_dataset(employee_dataset):
 
     df.drop(columns=['Gender', 'Department'], inplace=True)
 
-    # ------------- Encoding BusinessTravel and MaritalStatus -------------
+    # -- Encoding BusinessTravel and MaritalStatus --
 
     df['BusinessTravel'] = df['BusinessTravel'].map({'Non-Travel': 0, 'Travel_Rarely': 1, 'Travel_Frequently': 2})
     df['MaritalStatus'] = df['MaritalStatus'].map({'Single': 0, 'Married': 1, 'Divorced': 2})
 
-    # ------------- Mapping -------------
+    # -- Mapping --
     df['OverTime'] = (df['OverTime'].str.lower()).map({'no': 0, 'yes': 1})
     df['EducationField'] = df['EducationField'].map({'Other': 0, 'Life Sciences': 1, 'Medical': 2, 'Marketing': 3, 'Technical Degree': 4, 'Human Resources': 5})
     df['is_other_JobRole'] = (df['JobRole'] == 'Other').astype(int)
@@ -63,14 +63,7 @@ def clean_dataset(employee_dataset):
 # -----------------------------------------------------------------------------
 
 
-app = Flask(__name__)
-
-# ------------------------------- App Functions -------------------------------
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict/<employee_dataset>', methods=['GET' ,'POST'])
+# ------------------------------- Primary Tools -------------------------------
 def sample_predict(employee_dataset):
 
     raw_dataset_name = employee_dataset + '.csv'
@@ -86,11 +79,9 @@ def sample_predict(employee_dataset):
     y_test_proba = model.predict_proba(df)[:, 1]
     y_pred_binary = (y_test_proba >= 0.5).astype(int)
 
-    table = results(y_test_proba, y_pred_binary, dataset, raw_dataset)
+    dashboard_pred = results(y_test_proba, y_pred_binary, dataset, raw_dataset)
+    return dashboard_pred
 
-    return Response(table.to_html(), mimetype='text/html')
-
-@app.route('/ViewInsights/<employee_dataset>', methods=['GET', 'POST'])
 def combined_dataset(employee_dataset):
     raw_dataset_name = employee_dataset + '.csv'
     raw_dataset_directory = 'test_csv/' + raw_dataset_name
@@ -101,17 +92,31 @@ def combined_dataset(employee_dataset):
     results_table = pd.read_json('jsons/results_table.json')
     cols = results_table.iloc[:, [4, 5, 6]]
     combined_df = pd.concat([raw_dataset, cols], axis=1)
-
-    return Response(combined_df.to_html(), mimetype='text/html')
+    return combined_df
 # --------------------------------------------------------------------------------
 
+app = Flask(__name__)
 
-# ------------------------------- Testing Purposes -------------------------------
-@app.route('/show/<df>', methods=['GET', 'POST'])
-def show(df):
-    df = clean_dataset(df)
-    return df.sample(7).to_html()
+# -------------------------------- App Functions --------------------------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/<employee_dataset>/Raw', methods=['GET', 'POST'])
+def show_raw(employee_dataset):
+    raw_dataset_name = employee_dataset + '.csv'
+    raw_dataset_directory = 'test_csv/' + raw_dataset_name
+    raw_dataset = pd.read_csv(raw_dataset_directory)
+    return raw_dataset.sample(7).to_html()
 
+@app.route('/<employee_dataset>/Dashboard', methods=['GET' ,'POST'])
+def show_dashboard(employee_dataset):
+    dashboard = sample_predict(employee_dataset)
+    return Response(dashboard.to_html(), mimetype='text/html')
+
+@app.route('/<employee_dataset>/ViewInsights', methods=['GET', 'POST'])
+def show_view_insights(employee_dataset):
+    insights = combined_dataset(employee_dataset)
+    return Response(insights.to_html(), mimetype='text/html')
 
 # --------------------------------------------------------------------------------
 
